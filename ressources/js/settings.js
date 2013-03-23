bc2aa17a99ae233f41ecdc2556bb3b9acd24ec27
SaruManiac <sarumaniac@gmail.com>
2013-03-23 17:29:30 +0100

    Improvement of charts and map pages Addition of settings file
    - Addition of settings.js
    - Addition of the measurments units in the charts labels

---
+++ ressources/js/settings.js
@@ -0,0 +1,19 @@
+//Unités des mesures
+var settings = {};
+
+settings.tabUnites = { 	
+				"altitude" : "m",
+				"speedGPS" : "nd",
+				"luxAverage" : "lux",
+				"tempIn" : "&degC",
+				"tempOut" : "&degC",
+				"pressure" : "hPa"
+			};
+			
+//Tiles servers -> layerName : [ tileServerURL, subdomains, layerLabel]		
+settings.tabServers = {
+						"osmMap" : ["http://{s}.tile.osm.org/{z}/{x}/{y}.png", "", "OSM Map"],
+						"googleMap" : ["http://mt{s}.google.com/vt/v=w2.106&x={x}&y={y}&z={z}&s=", "0123", "Google Map"],
+						"googleHybrid" : ["http://mt{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}", "0123", "Google Hybrid"],
+						"mapQuest" : ["http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", "1234", "Map Quest"]
+						};
