950d31aebea82b02cf5f31dbf187172f09863b38
SaruManiac <sarumaniac@gmail.com>
2013-04-05 21:11:10 +0200

    Improvement of the POI addition Update of the POI display function
    - Addition of settings.POI
    - Improvement of the way the POI are added

--- ressources/js/settings.js
+++ ressources/js/settings.js
@@ -1,8 +1,8 @@
 
 var settings = {};
 
-//Unités des mesures
-settings.tabUnites = { 	
+//Measurment units
+settings.tabUnites = { //"unitName" : "unit" 	
 				"sensor1" : "unit",
 				"sensor2" : "unit",
 				"sensor3" : "unit",
@@ -14,10 +14,15 @@ settings.tabUnites = {
 				"voltage" : "V"
 			};
 			
-//Tiles servers -> layerName : [ tileServerURL, subdomains, layerLabel]		
-settings.tabServers = {
+//Tiles server list
+settings.tabServers = { //"layerName" : ["tileServerURL", "subdomains", "layerLabel"]	
 						"osmMap" : ["http://{s}.tile.osm.org/{z}/{x}/{y}.png", "", "OSM Map"],
 						"googleMap" : ["http://mt{s}.google.com/vt/v=w2.106&x={x}&y={y}&z={z}&s=", "0123", "Google Map"],
 						"googleHybrid" : ["http://mt{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}", "0123", "Google Hybrid"],
 						"mapQuest" : ["http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", "1234", "Map Quest"]
 						};
+//POI list			
+settings.POI = { //"pointName" : [Lat, Lng, "popupText"]
+				"iut" : [44.91568, 4.91524, "IUT de Valence"],
+				"pointArbitraire" : [44.91970, 4.9200, "Point arbitraire <br/> (Ne sert &agrave rien)"],
+				};
