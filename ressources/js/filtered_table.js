0e263c50e3a0ebded9a4a809b29d4bb29446f8ee
Mathieu Agopian <mathieu.agopian@gmail.com>
2014-03-26 22:13:36 +0100

    move the (very short) code from filtered_table.js to html

--- ressources/js/filtered_table.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function updateTable(data) {
-  var values = [];
-  for (var propName in settings.fieldLabels) {
-    values.push(data[propName]);
-  };
-  $('<tr></tr>').append('<td>' + values.join('</td><td>') + '</td>').insertAfter('#headers');
-}
