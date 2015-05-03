3aaf1433179d66338a0f29888451798d8a7de5d7
sebastienjean <baz.jean@gmail.com>
2015-05-03 17:49:35 +0200

    Fixed some "2014" stuff in HTML files
    - some references to 2014 were remaining ...

--- ressources/js/map.js
+++ ressources/js/map.js
@@ -10,7 +10,7 @@ function setupMap(name, mapServer, maxZ, minZ) {
   L.tileLayer(mapServer,
             {
               subdomains : '0123',
-              attribution : 'UBPE 2014 - IUT Valence',
+              attribution : 'UBPE 2015 - IUT Valence',
               maxZoom : maxZ,
               minZoom : minZ
             }).addTo(map);
